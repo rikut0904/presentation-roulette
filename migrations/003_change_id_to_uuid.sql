093ce44060c866c7a7a5478ef6ab0b17741820f4
rikut0904 <rkt9tuba4robot@gmail.com>
2026-04-19 16:33:22 +0900

    fix/migration

--- migrations/003_change_id_to_uuid.sql
+++ migrations/003_change_id_to_uuid.sql
@@ -5,9 +5,8 @@
 ALTER TABLE roulette_models DROP CONSTRAINT IF EXISTS roulette_models_user_uid_fkey;
 
 -- Change ID column type. We'll use VARCHAR(36) to store UUIDs.
--- Note: This will effectively clear existing integer IDs if not cast carefully, 
--- but since we are in development, a clean sweep or simple cast is fine.
-ALTER TABLE roulette_models ALTER COLUMN id TYPE VARCHAR(36);
+-- Note: PostgreSQL requires an explicit USING clause for this cast.
+ALTER TABLE roulette_models ALTER COLUMN id TYPE VARCHAR(36) USING id::VARCHAR(36);
 
 -- Re-add foreign key
 ALTER TABLE roulette_models ADD CONSTRAINT roulette_models_user_uid_fkey 
