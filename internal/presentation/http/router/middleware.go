5912d1de924beb2cda39a413addb356a5ef87185
rikut0904 <rkt9tuba4robot@gmail.com>
2026-04-19 23:38:26 +0900

    fix/entity

--- internal/presentation/http/router/middleware.go
+++ internal/presentation/http/router/middleware.go
@@ -3,17 +3,31 @@ package router
 import (
 	"net/http"
 
+	"github.com/gorilla/sessions"
 	"github.com/labstack/echo-contrib/session"
 	"github.com/labstack/echo/v4"
 )
 
 func AuthMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
-		sess, _ := session.Get("session", c)
-		userUID, ok := sess.Values["uid"].(string)
-		if !ok || userUID == "" {
+		sess, err := session.Get("session", c)
+		if err != nil {
 			return echo.NewHTTPError(http.StatusUnauthorized, "ログインしてください")
 		}
+
+		userUID, ok := sess.Values["uid"].(string)
+		if !ok || userUID == "" {
+			delete(sess.Values, "uid")
+			sess.Options = &sessions.Options{
+				Path:     "/",
+				HttpOnly: true,
+				MaxAge:   -1,
+				SameSite: http.SameSiteLaxMode,
+			}
+			_ = sess.Save(c.Request(), c.Response())
+			return echo.NewHTTPError(http.StatusUnauthorized, "ログインしてください")
+		}
+
 		c.Set("userUID", userUID)
 		return next(c)
 	}
